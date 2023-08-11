f67ba0ad586bb23d7b5cef094f282629b7c74dc4
Sibusiso Mvundla <69355799+SbudaRabbit@users.noreply.github.com>
2023-08-11 10:14:00 +0200

    Add files via upload

---
+++ comonents/textArea.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+function TextField (){
+    return(
+<Text></Text>
+    )
+}
+export default TextField;
